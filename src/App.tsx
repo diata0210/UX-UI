1acf97300130d8dda828b74501608297679d47a1
Tuan <thaituandang0210@gmail.com>
2024-05-10 19:45:50 +0700

    init code

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <Button variant="outline" size="sm">Button</Button>
+    </div>
+  )
+}
