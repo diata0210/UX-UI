991796e1450d8bf4343799c54d00c418bfc11364
Kkhokho <khoi.mm210492@gmail.com>
2024-05-28 16:57:04 +0700

    fix evalute

--- src/page/EvaluationPage.tsx
+++ src/page/EvaluationPage.tsx
@@ -1,12 +1,12 @@
 import { BarGraph } from "@/components/ui/barGraph";
 import { LineGraph } from "@/components/ui/lineGraph";
-import { Radio } from 'antd';
+import { Radio, Select } from "antd";
 import { useState } from "react";
 
 type SizeType = {};
 export default function EvaluationPage() {
-  const [size, setSize] = useState<SizeType>('week'); // default is 'middle'
-  const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const [size, setSize] = useState<SizeType>("week"); // default is 'middle'
+  const labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
   const options = {
     responsive: true,
@@ -15,11 +15,11 @@ export default function EvaluationPage() {
         position: "bottom" as const,
         labels: {
           boxWidth: 20,
-        }
+        },
       },
       title: {
         display: false,
-        text: 'Sơ đồ biểu diễn thời gian hoàn thành task dự kiến và thực tế trong tuần qua',
+        text: "Sơ đồ biểu diễn thời gian hoàn thành task dự kiến và thực tế trong tuần qua",
       },
     },
   };
@@ -32,31 +32,29 @@ export default function EvaluationPage() {
     labels,
     datasets: [
       {
-        label: 'Thời gian dự kiến',
+        label: "Thời gian dự kiến",
         data: labels.map(() => getRandomNumber(0, 24)),
-        backgroundColor: '#4EB9EA',
-
+        backgroundColor: "#4EB9EA",
       },
       {
-        label: 'Thời gian thực tế',
+        label: "Thời gian thực tế",
         data: labels.map(() => getRandomNumber(0, 24)),
-        backgroundColor: '#116783',
+        backgroundColor: "#116783",
       },
     ],
   };
 
-  const labelsLine = ['1', '5', '10', '15', '20', '25', '30']
+  const labelsLine = ["1", "5", "10", "15", "20", "25", "30"];
 
   const optionsLine = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'bottom' as const,
+        position: "bottom" as const,
       },
       title: {
         display: false,
-        text: 'Chart.js Line Chart',
-
+        text: "Chart.js Line Chart",
       },
     },
   };
@@ -65,81 +63,80 @@ export default function EvaluationPage() {
     labels: labelsLine,
     datasets: [
       {
-        label: 'Thời gian dự kiến',
+        label: "Thời gian dự kiến",
         data: labelsLine.map(() => getRandomNumber(20, 100)),
-        borderColor: '#4EB9EA',
-        backgroundColor: '#4EB9EA',
+        borderColor: "#4EB9EA",
+        backgroundColor: "#4EB9EA",
       },
       {
-        label: 'Thời gian thực tế',
+        label: "Thời gian thực tế",
         data: labelsLine.map(() => getRandomNumber(20, 100)),
-        borderColor: '#116783',
-        backgroundColor: '#116783',
+        borderColor: "#116783",
+        backgroundColor: "#116783",
       },
     ],
   };
 
-
-
-  const [timeDisplay, setTimeDisplay] = useState('week')
-
-
+  const [timeDisplay, setTimeDisplay] = useState("week");
 
   return (
-    <div
-      className="  setting-container flex-col flex rounded-[10px] border-2 border-[#d9d9d9] mt-[10px] min-h-[80vh] w-full "
-    >
-      <div className='breadcrum relative'>
-        <div className='flex-grow-0 flex-shrink-0 text-[36px] font-semibold text-left text-blue-500 text-4xl font-inter '>
-          Đánh giá
-        </div>
+    <div className="setting-container flex-col gap-[20px] flex rounded-[10px] border-2 border-[#d9d9d9] w-full ">
+      <div className="flex-grow-0 flex-shrink-0 text-[36px] font-semibold text-left text-blue-500 text-4xl font-inter ">
+        Đánh giá
       </div>
-      <div className="content w-full relative  flex flex-col gap-[15px] mt-[10px] ">
-        <div>
-
-          <Radio.Group value={size} onChange={(e) => { setTimeDisplay(e.target.value), setSize(e.target.value) }}>
-            <Radio.Button value="week">Tuần</Radio.Button>
-            <Radio.Button value="month">Tháng</Radio.Button>
-          </Radio.Group>
-        </div>
-        <div className='w-full mx-auto flex justify-between flex-col '>
-          {timeDisplay === 'week' ?
-            <>
-              <div className="flex flex-row gap-[60px] items-center">
-                <div className="max-w-[850px] ">
-                  <div className='mx-auto font-medium text-[20px] w-full mb-[20px]'>Sơ đồ biểu diễn thời gian hoàn thành task dự kiến và thực tế trong tuần qua</div>
-                  <BarGraph options={options} data={data} />
-
+      <div className="w-full flex flex-row items-center justify-between gap-[28px]">
+        <div className="w-full flex flex-col gap-[15px] mt-[10px] ">
+          <div className="w-full flex flex-row items-center justify-between">
+            <Select className="w-[200px] " defaultValue="Tiếng Nhật">
+              <Select.Option value="Tiếng Nhật">Tiếng Nhật</Select.Option>
+              <Select.Option value="IT">IT</Select.Option>
+              <Select.Option value="UX-UI">UX-UI</Select.Option>
+            </Select>
+            <Radio.Group
+              className="h-[54px]"
+              value={size}
+              onChange={(e) => {
+                setTimeDisplay(e.target.value), setSize(e.target.value);
+              }}
+            >
+              <Radio.Button value="week">Tuần</Radio.Button>
+              <Radio.Button value="month">Tháng</Radio.Button>
+            </Radio.Group>
+          </div>
+          <div className="w-full mx-auto flex flex-row items-center justify-between">
+            {timeDisplay === "week" ? (
+              <div className="flex items-center justify-between gap-[30px]">
+                <img src="/KPI.png" className="w-[250px]" />
+                <div className="flex flex-row md:flex-col gap-[60px] items-center">
+                  <div className="max-w-[500px] h-[312px]">
+                    <div className="flex w-full justify-center font-medium text-[20px] text-[#3A99F1] mb-[20px]">
+                      Hiệu suất làm việc
+                    </div>
+                    <BarGraph options={options} data={data} />
+                  </div>
                 </div>
-                <div className="flex flex-col gap-[50px] w-[350px]">
-                  <img src="/report1.png" alt="warning" />
-                  <img src="/report2.png" alt="khen" />
-                </div>
-
               </div>
-
-            </>
-            :
-            <>
-
-              <div className="flex flex-row gap-[60px] items-center">
-                <div className="max-w-[850px]">
-                <div className='mx-auto font-medium text-[20px] w-full mb-[20px]'>Sơ đồ biểu diễn thời gian hoàn thành task dự kiến và thực tế trong tháng qua</div>
-                <LineGraph options={optionsLine} data={dataLine} />
-
+            ) : (
+              <>
+                <div className="flex flex-row gap-[60px] items-center">
+                  <div className="max-w-[850px]">
+                    <div className="mx-auto font-medium text-[20px] w-full mb-[20px]">
+                      Hiệu suất làm việc
+                    </div>
+                    <LineGraph options={optionsLine} data={dataLine} />
+                  </div>
                 </div>
-                <div className="flex flex-col gap-[50px] w-[350px]">
-                  <img src="/report1.png" alt="warning" />
-                  <img src="/report2.png" alt="khen" />
-                </div>
-
-              </div>
-            </>
-          }
-
+              </>
+            )}
+          </div>
         </div>
 
+        <div className="flex flex-col max-w-[288px] gap-[34px]">
+          <img src="/report1.png" alt="" />
+          <img src="/report2.png" alt="" />
+          <img src="/report2.png" alt="" />
+        </div>
       </div>
     </div>
-  )
+  );
 }
